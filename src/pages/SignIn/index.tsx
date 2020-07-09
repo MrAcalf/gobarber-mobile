a34ba43c0f33956898f89d14355a381453be2c0a
mracalf <antonio.lima16@fatec.sp.gov.br>
2020-07-09 18:16:34 -0300

    Navigation between login and logon pages is working

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -8,6 +8,7 @@ import {
   Keyboard
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
 import {
   Container,
@@ -24,6 +25,7 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 const SignIn: React.FC = () => {
+  const navigation = useNavigation();
   return (
     <>
       <KeyboardAvoidingView
@@ -59,7 +61,7 @@ const SignIn: React.FC = () => {
           </Container>
         </ScrollView>
       </KeyboardAvoidingView>
-      <CreateAccountButton onPress={() => {}}>
+      <CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
         <Icon name='log-in' size={20} color='#ff9000' />
         <CreateAccountButtonText>
           Criar uma conta
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,10 +1,66 @@
 import React from 'react';
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  View,
+  ScrollView
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
-import { Container } from './styles';
+import {
+  Container,
+  Title,
+  BackToSignIn,
+  BackToSignInText
+} from './styles';
+import { useNavigation } from '@react-navigation/native';
+
+import logoImg from '../../assets/logo.png';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const SignUp: React.FC = () => {
+  const navigation = useNavigation();
   return (
-    <Container />
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={
+          Platform.OS === 'ios' ? 'padding' : undefined
+        }
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps='handled'
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+
+            <View>
+              <Title>Crie sua conta</Title>
+            </View>
+
+            <Input name='name' icon='user' placeholder='Nome'/>
+            <Input name='email' icon='mail' placeholder='E-mail'/>
+
+            <Input name='password' icon='lock' placeholder='Senha'/>
+
+            <Button onPress={() => {
+              console.log('ok');
+            }}>Entrar</Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      <BackToSignIn onPress={() => navigation.goBack()}>
+        <Icon name='arrow-left' size={20} color='#fff' />
+        <BackToSignInText>
+          Voltar para logon
+        </BackToSignInText>
+      </BackToSignIn>
+    </>
   );
 };
 
